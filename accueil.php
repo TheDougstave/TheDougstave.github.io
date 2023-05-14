88ee4730e53a92b899a9e73e602e4a9ff3221c04
TheDougstave <121986293+TheDougstave@users.noreply.github.com>
2023-05-14 19:30:03 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon PortFolio</title>
+    <title>Roule Ma Poule !</title>
 </head>
 <body>
-    
+    header("location:mainmenu.php")
 </body>
 </html>
